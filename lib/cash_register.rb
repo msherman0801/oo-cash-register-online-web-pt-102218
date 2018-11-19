ee996b7a0fd426701e9642dec926738ece638877
Matthew Sherman <msherman0801@gmail.com>
2018-11-19 23:17:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :discount
 
+  def initialize(discount=nil)
+    @total = 0
+    if discount != nil
+      @discount = discount
+    end
+  end 
+  
+
+  def add_item(title, price, quantity=1)
+    if quantity > 1
+      @total += (price*quantity)
+    else
+      @total += price 
+    end
+  end
+  
+  def apply_discount
+    self.discount = sel.fdiscount / 100
+    binding.pry
+    @total = @total * @discount
+    @total
+  end
+  
+  
+end
